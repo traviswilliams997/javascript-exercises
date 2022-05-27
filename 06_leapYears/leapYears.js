1a1796e7a67181f4a8251c95d4c8e3220c696897
Travis Williams <traviswilliams997@gmail.com>
2022-05-27 17:58:05 -0500

    Add solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year % 4 == 0 && year % 400 == 0 ){
+        return true;
+    }
+
+    if(year % 4 == 0 && year % 100 == 0 && year % 400 != 0){
+        return false;
+    }
+    if(year % 4 == 0){
+        return true;
+    }
+    return false;
 
 };
 
