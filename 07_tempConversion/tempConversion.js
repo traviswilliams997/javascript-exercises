01a9e93c64a8c60fd43e8ae72e4973142c3008a2
Travis Williams <traviswilliams997@gmail.com>
2022-05-27 18:15:30 -0500

    Add comments

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
 const ftoc = function(temp) {
+  //conversion
   let newTemp  = (temp - 32 )  * (5/9);
 
+  //round to 1 decimal
   return Math.round(newTemp * 10) / 10 ;
 
 };
 
 const ctof = function(temp) {
-
+  //conversion
   let newTemp = (temp * (9/5) )  + 32;
+
+  //round to 1 decimal
   return Math.round(newTemp * 10) / 10 ;
 
 };
