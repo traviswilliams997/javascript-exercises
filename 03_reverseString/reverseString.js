52af385989e689109cb68d38046631e04c4dfe95
Travis Williams <traviswilliams997@gmail.com>
2022-05-27 16:38:04 -0500

    Add solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = '';
+
+    //start from last char of orginal string and each add char to new string
+    for(let i = string.length -1; i >= 0; i-- ){
+        newString += string.charAt(i);
+    }
+    return newString;
+
 
 };
 
