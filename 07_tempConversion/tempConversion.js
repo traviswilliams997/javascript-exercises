243fcae1fb03f9b07c1241ecf99891e8bdf1f339
Travis Williams <traviswilliams997@gmail.com>
2022-05-27 18:14:19 -0500

    Add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let newTemp  = (temp - 32 )  * (5/9);
+
+  return Math.round(newTemp * 10) / 10 ;
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+
+  let newTemp = (temp * (9/5) )  + 32;
+  return Math.round(newTemp * 10) / 10 ;
 
 };
 
