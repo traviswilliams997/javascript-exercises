642d77f5489ee873812a0bcb768397d936c50d21
Travis Williams <traviswilliams997@gmail.com>
2022-05-27 17:33:17 -0500

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+ 
+//for all arguments  entered after orginal array
+    for (const a of args) {
+        //loop through original array  
+        for(let i = 0; i < array.length; i++){
+            //check if equal to argument we wish to delete
+            if(array[i] ===  a ){    
+                //remove only the argument to be deleted
+                array.splice(i, 1);
+                
+            }
+
+            
+        }
+    }
+   
+    return  array;
 
 };
 
