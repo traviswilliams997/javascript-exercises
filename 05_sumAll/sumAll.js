6d3681359e694ebfef3f9842dc1ee4f4a746f04e
Travis Williams <traviswilliams997@gmail.com>
2022-05-27 17:48:04 -0500

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    //negative number check
+    if(num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    //non number check
+    if(typeof num1 !== 'number'  || typeof num2 !== 'number' ){
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    //num 2 greater than num 1 check
+    if( num1 < num2){
+        for(let i = num1; i <= num2; i++ ){
+            sum += i;
+        }
+    }else{
+        for(let i = num2; i <= num1; i++ ){
+            sum += i;
+        }
+
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
