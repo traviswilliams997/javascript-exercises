679a9a12ee31287ef4d23b0f66e6adfe0a243956
Travis Williams <traviswilliams997@gmail.com>
2022-05-29 16:53:54 -0500

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    //add age value
+    people.map(person => { 
+              if(person.yearOfDeath == undefined){
+                person.age = currentYear - person.yearOfBirth;
+              }else{
+                person.age = person.yearOfDeath - person.yearOfBirth;
+              }
+            });
+
+    //sort oldest to youngest      
+     people.sort((a,b) => a.age > b.age ? -1: 1 );
+
+    //return name of first person
+    return people[0];
 
 };
 
