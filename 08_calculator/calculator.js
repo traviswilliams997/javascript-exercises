b2cec6a07eb4828a8900b954fac1f339020d8053
Travis Williams <traviswilliams997@gmail.com>
2022-05-29 09:00:54 -0500

    Add solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+   return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+   return arr.reduce((sums, value) => {return sums + value;}, 0);        
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((mul, value) => {return mul * value;}, 1);    
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let power = 1;
+  for(let i = 1; i <= b; i++){
+    power *= a;
+  }
+  return power;
+  
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }
+  let ans = 1;
+  for(let i = 1; i <= num; i++){
+     ans *= i;
+  }
+  return ans;
+
+
 	
 };
 
