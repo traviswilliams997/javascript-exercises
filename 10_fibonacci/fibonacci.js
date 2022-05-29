60021ebafe1a4d319141dcb86a104d0e4c113e3f
Travis Williams <traviswilliams997@gmail.com>
2022-05-29 16:00:26 -0500

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    let n = 2;
+    let a = 1;
+    let b = 1;
+    if(nth < 0){
+        return "OOPS";
+    }
+    
+
+    for(let i = n; i < nth; i++){
+        let temp = a + b;
+        a = b;
+        b = temp;
+
+    }
+    return b;
+
+
 
 };
 
